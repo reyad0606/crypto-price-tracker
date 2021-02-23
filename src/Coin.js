4db489839d3e868c7d21ab08064b476061aaaf4c
RM <70502707+reyad0606@users.noreply.github.com>
2021-02-22 19:09:54 -0500

    Commit

---
+++ src/Coin.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Coin.css';
+
+const Coin = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+}) => {
+  return (
+    <div className='coin_container'>
+      <div className='coin_row'>
+        <div className='coin'>
+          <img src={image} alt='crypto' />
+          <h1>{name}</h1>
+          <p className='coin_symbol'>{symbol}</p>
+        </div>
+        <div className='coin_data'>
+          <p className='coin_price'>${price}</p>
+          <p className='coin_volume'>{volume.toLocaleString()}</p>
+          {priceChange < 0 ? (
+            <p className='coin_percent red'>{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className='coin_percent green'>{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className='coin_marketcap'>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
